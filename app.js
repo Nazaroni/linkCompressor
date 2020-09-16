2ed20f5be3c6600b264197e252b0fd16b2df71fa
Nazar <nazar.malyy@gmail.com>
2020-09-16 09:48:20 +0200

    Update app.js
    eslint update

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const path          = require( 'path' );
-const express       = require( 'express' );
-const config        = require( 'config' );
-const mongoose      = require( 'mongoose' );
-const myAuthRoutes  = require( './routes/auth.routes' );
-const myLinkRouter  = require( './routes/link.routes' );
-const myRedirect    = require( './routes/redirect.routes' );
+const path          = require( 'path' )
+const express       = require( 'express' )
+const config        = require( 'config' )
+const mongoose      = require( 'mongoose' )
+const myAuthRoutes  = require( './routes/auth.routes' )
+const myLinkRouter  = require( './routes/link.routes' )
+const myRedirect    = require( './routes/redirect.routes' )
 
 const app   = express();
-const PORT  = config.get( 'port' ) || 5000;
+const PORT  = config.get( 'port' ) || 5000
 
 // middlewares
-app.use( express.json( { extended: true } ) );
+app.use( express.json( { extended: true } ) )
 
-app.use( '/api/auth', myAuthRoutes );
-app.use( '/api/link', myLinkRouter );
-app.use( '/t', myRedirect );
+app.use( '/api/auth', myAuthRoutes )
+app.use( '/api/link', myLinkRouter )
+app.use( '/t', myRedirect )
 
 if ( process.env.NODE_ENV === 'production' ) {
-  app.use( '/', express.static( path.join( __dirname, 'client', 'build') ) );
+  app.use( '/', express.static( path.join( __dirname, 'client', 'build') ) )
 
   app.get( '*', ( req, res ) => {
-    res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html') );
-  });
+    res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html') )
+  })
 }
 
 /**
@@ -35,12 +35,12 @@ const start = async () => {
       useCreateIndex: true,
     });
 
-    app.listen( PORT, () => { console.log( `App has been started on port${ PORT }...` ); });
+    app.listen( PORT, () => { console.log( `App has been started on port${ PORT }...` ) })
   }
   catch ( errStart ) {
-    console.log( 'App.js -> start -> Server error...', errStart.message );
-    process.exit( 1 );
+    console.log( 'App.js -> start -> Server error...', errStart.message )
+    process.exit( 1 )
   }
 };
 
-start();
+start()
